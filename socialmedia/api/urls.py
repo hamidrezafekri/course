2926c017e8681a94bd0b1c880e578a027b488dd5
hamidrezafekr <hamidrezafekri.dev@gmail.com>
2023-09-19 15:11:16 +0330

    change in project

--- socialmedia/api/urls.py
+++ socialmedia/api/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('blog/', include(('socialmedia.blog.urls', 'blog'))),
+    path('blog/', include(('socialmedia.users.urls', 'users'))),
+    path('blog/', include(('socialmedia.authentication.urls', 'auth'))),
 ]
