956926c00f983a0b4c93abb0de04ac6dfaa6e84d
hamidrezafekr <hamidrezafekri.dev@gmail.com>
2023-09-20 13:31:02 +0330

    add test in project

---
+++ socialmedia/tests/factories.py
@@ -0,0 +1,51 @@
+import factory
+
+from socialmedia.tests.utils import faker
+from socialmedia.users.models import (
+    BaseUser,
+    Profile,
+)
+from socialmedia.blog.models import (
+    Subcription,
+    Post,
+)
+from django.utils import timezone
+
+
+class BaseUserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = BaseUser
+
+    email = factory.Iterator('fr@gmail.com', 'it@gmail.com', 'es@gmail.com')
+    password = factory.PostGenerationMethodCall('set_password', 'admin')
+
+
+class SubscriptionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Subcription
+
+    target = factory.SubFactory(BaseUserFactory)
+    subscriber = factory.SubFactory(BaseUserFactory)
+
+
+class PostFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Post
+
+    title = factory.LazyAttribute(lambda _: f'{faker.unique.company()}')
+    content = factory.LazyAttribute(lambda _: f'{faker.unique.company()}')
+    slug = factory.LazyAttribute(lambda _: f'{faker.unique.company()}')
+    created_at = factory.LazyAttribute(lambda _: f'{timezone.now()}')
+    updated_at = factory.LazyAttribute(lambda _: f'{timezone.now()}')
+    author = factory.SubFactory(BaseUserFactory)
+
+
+class ProfileFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Profile
+
+    user = factory.SubFactory(BaseUserFactory)
+    posts_count = factory.LazyAttribute(lambda _: 0)
+    subscription_count = factory.LazyAttribute(lambda _: 0)
+    subscriber_count = factory.LazyAttribute(lambda _: 0)
+    bio = factory.LazyAttribute(lambda _:f'{faker.unique.company()}')
